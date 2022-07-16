e8e08faf7b0ea9621d26a8c79c97d6216d33fe42
KoSHeroff <dev.kosher@gmail.com>
2022-07-16 04:10:08 +0400

    modified:   src/App.css 	modified:   src/App.jsx 	new file:   src/assets/logo.svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import logo from './assets/logo.svg';
+
 import './App.css'
 
 function App() {
 
-  return (
+  return  (
     <>
+      <div className='section greetings'>
+         <div className='header'>
+          <div className='header-logo'>
+            <div className='header-logo-icon'>
+              <img src={logo} alt="logo" />
+            </div>
+            <div className='header-logo-name'>CloudBudget</div>
+          </div>
+          <div className='header-menu'>
+            <div className='header-menu-element'>Обзор</div>
+            <div className='header-menu-element'>Особенности</div>
+            <div className='header-menu-element'>Технологии</div>
+            <div className='header-menu-element'>Контакты</div>
+            <div className='header-menu-element'>Войти</div>
+          </div>
+          <div className='header-profile'>
+            <div className='btn btn-fill'>
+              Вход
+            </div>
+          </div>
+         </div>
+      </div>
     </>
   )
 }
